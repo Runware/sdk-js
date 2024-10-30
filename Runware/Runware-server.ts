c80026739e4ac555c8bf5af57a2a95c51eb32e62
gorantosicpf <goran.tosic@runware.ai>
2024-10-30 11:44:26 +0100

    Bug with resetConnection() call in connect() fix

--- Runware/Runware-server.ts
+++ Runware/Runware-server.ts
@@ -132,9 +132,9 @@ export class RunwareServer extends RunwareBase {
   protected resetConnection = () => {
     if (this._ws) {
       this._listeners.forEach((list) => {
-        let _a;
-        (_a = list == null ? void 0 : list.destroy) == null ? void 0 : _a.call(list);
+        list?.destroy?.();
       });
+
       this._ws.removeAllListeners(); // Remove all listeners
       if (this._ws.readyState === 1) {
         this._ws.terminate();
